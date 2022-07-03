9b39ed75c747fca76c7b28b6110287291eba21c0
georgeKorbut <georgekorbut@gmail.com>
2022-07-03 10:56:28 -0400

    day 2 tip calculator
    day 2 complete

---
+++ Day1to10/Day1to10/day2.py
@@ -0,0 +1,44 @@
+#tip calculator project
+
+#bmi calculator
+# height = input("enter your height in m:")
+# weight = input("enter your weight in kg:")
+
+# print(type(height))
+# print(type(weight))
+
+# height_float = float(height)
+# weight_float = float(weight)
+
+# print(height_float, weight_float)
+
+# bmi = weight_float / (height_float ** 2)
+# print(int(bmi))
+
+#day 2.3 life in weeks, f-string exercise
+# age = input("What is your current age?")
+# years_remaining = 90 - int(age)
+# weeks_in_year = 52
+# months_in_year = 12
+# days_in_year = 365
+
+# #math
+# days_remaining = years_remaining * days_in_year
+# weeks_remaining  = years_remaining * weeks_in_year
+# months_remaining = years_remaining * months_in_year
+
+# print(f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left.")
+
+#tip calculator, day 2 final project
+print("Welcome to the tip calculator")
+bill_no_tip = int(input("What was the total bill?"))
+tip_percent = int(input("What percentage tip would you like to give? 10, 12, or 15?"))
+num_people = int(input("How many people to split the bill?"))
+
+bill_with_tip = bill_no_tip * (1 + tip_percent / 100)
+bill_per_person = round(bill_with_tip / num_people, 2)
+
+print(f"Each person should pay: ${bill_per_person}")
+
+
+
