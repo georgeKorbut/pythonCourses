2cf1f82b8c2e3b48d350005e7789ebccd168afc6
georgeKorbut <georgekorbut@gmail.com>
2022-07-03 09:58:19 -0400

    python day 1
    lesson completed

--- Day1to10/Day1to10/day1.py
+++ Day1to10/Day1to10/day1.py
@@ -1,2 +1,28 @@
-print("Hello World")
+"""
+print("Hello World!")
 
+#string concatenation
+print("String concatenation is done with the \"+\" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+print("hello " + "world")
+"""
+#input function 
+# print("Hello " + input("What is your name?"))
+
+#day 1.3
+# print( len( input("What is your name?") ) )
+
+# name = "Jack"
+# print(name)
+
+# name = "Joe"
+# print(name)
+
+#band name generator
+
+print("Hello there")
+city = input("What city did you grow up in?\n")
+pet = input("What is your pets name?\n")
+print("Your band name is " + city + " " + pet)
